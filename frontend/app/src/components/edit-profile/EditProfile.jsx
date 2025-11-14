403705d7280934bcfd9324a162b9c3b11d48874b
Cronos962 <sanskarrajput962@gmail.com>
2025-11-14 11:42:48 +0530

    Edit profile ui

--- frontend/app/src/components/edit-profile/EditProfile.jsx
+++ frontend/app/src/components/edit-profile/EditProfile.jsx
@@ -4,34 +4,33 @@ const EditProfile = () => {
     return (
         <div className='w-50 mx-auto'>
             <h1 className='my-4'>Edit Profile</h1>
-            <form class="row g-3">
-                <div class="col form-floating mb-3">
-                    <input type="text" class="form-control" id='floatingInput' placeholder="First name" aria-label="First name" />
-                    <label for="floatingInput">First Name</label>
+            <form className="row g-3">
+                <div className="col form-floating mb-3">
+                    <input type="text" className="form-control" id='firstNameInput' placeholder="First name" aria-label="First name" required />
+                    <label htmlFor="firstNameInput">First Name</label>
                 </div>
-                <div class="col form-floating mb-3">
-                    <input type="text" class="form-control" id='floatingInput' placeholder="Last name" aria-label="Last name" />
-                    <label for="floatingInput">Last Name</label>
+                <div className="col form-floating mb-3">
+                    <input type="text" className="form-control" id='lastNameInput' placeholder="Last name" aria-label="Last name" required />
+                    <label htmlFor="lastNameInput">Last Name</label>
                 </div>
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                    <label for="floatingInput">Email address</label>
+                <div className="form-floating mb-3">
+                    <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" required />
+                    <label htmlFor="emailInput">Email address</label>
                 </div>
-                <div class="form-floating mb-3">
-                    <input type="tel" minLength={10} maxLength={10} class="form-control" id="floatingInput" placeholder=""  />
-                    <label for="floatingInput">Mobile</label>
+                <div className="form-floating mb-3">
+                    <input type="tel" minLength={10} maxLength={10} className="form-control" id="mobileInput" placeholder="" required />
+                    <label htmlFor="mobileInput">Mobile</label>
                 </div>
-                <div class="form-floating mb-3">
-                    <input type="date" class="form-control" id="floatingInput" placeholder="" />
-                    <label for="floatingInput">Date of birth</label>
+                <div className="form-floating mb-3">
+                    <input type="date" className="form-control" id="dobInput" placeholder="" required />
+                    <label htmlFor="dobInput">Date of birth</label>
                 </div>
 
-
-                    <button type="button" class="btn btn-success w-25">Save</button>
+                <button type="submit" className="btn btn-success w-25">Save</button>
 
             </form>
         </div>
     )
 }
 
-export default EditProfile
+export default EditProfile;
