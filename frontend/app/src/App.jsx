f1276d2a2ee73e0cb97f7f9cbaa710529b43fe75
Cronos962 <sanskarrajput962@gmail.com>
2025-11-14 12:41:04 +0530

    Review card ui

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -6,14 +6,17 @@ import Navbar from './components/navbar/Navbar'
 import ChangePassword from './components/change-password/ChangePassword'
 import EditProfile from './components/edit-profile/EditProfile'
 import EditReview from './components/edit-review/EditReview'
+import ReviewCard from './components/review-card/ReviewCard'
 const App = () => {
   return (
     <div>
-      <SignUp/>
       {/* <Navbar /> */}
+      <ReviewCard/>
+      {/* <SignUp/> */}
+      {/* <Login/> */}
       {/* <ChangePassword /> */}
       {/* <EditProfile/> */}
-      <EditReview/>
+      {/* <EditReview/> */}
     </div>
 
   )
--- frontend/app/src/components/login-page/Login.jsx
+++ frontend/app/src/components/login-page/Login.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import './Login.css'
+
 const Login = () => {
     return (
-        <div className='login-form w-25 mx-auto mt-lg-5'>
+        <div className='w-25 mx-auto mt-lg-5 p-4 border rounded-4'>
             <div className='text-center'>
             <h1>Sign in</h1>
 
             </div>
-            <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                    <label for="floatingInput">Email address</label>
-            </div>
-            <div className="form-floating">
-                <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                    <label for="floatingPassword">Password</label>
-            </div>
-            <button type="button" class="btn btn-success my-4 w-100">Log in</button>
+            <form>
+                <div className="form-floating mb-3">
+                    <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" required />
+                        <label htmlFor="emailInput">Email address</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="passwordInput" placeholder="Password" required />
+                        <label htmlFor="passwordInput">Password</label>
+                </div>
+                <button type="submit" className="btn btn-success my-4 w-100">Log in</button>
+            </form>
             <div className='text-center'>
                 <h6>Dont have an account?  <a href="">Signup</a></h6>
                 <h6><a href="">Forgot Password</a></h6>
@@ -24,4 +26,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Login;
